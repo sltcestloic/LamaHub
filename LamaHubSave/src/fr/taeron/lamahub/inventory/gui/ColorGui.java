8795a7e501496f06ebf2735b1deb9ae1cc00687c
Taeron <trotibug@hotmail.fr>
2017-07-14 19:16:35 +0200

    gneu gneu utf 8 gneu gneu

--- LamaHubSave/src/fr/taeron/lamahub/inventory/gui/ColorGui.java
+++ LamaHubSave/src/fr/taeron/lamahub/inventory/gui/ColorGui.java
@@ -12,17 +12,17 @@ public class ColorGui {
 	private static Inventory i;
 	
 	public ColorGui(){
-		i = Bukkit.createInventory(null, 18, "§aChoisis une couleur");
-		i.setItem(0, new ItemBuilder(Material.WOOL).data((short)1).displayName("§6Orange").build());
-		i.setItem(1, new ItemBuilder(Material.WOOL).data((short)11).displayName("§1Bleu").build());
-		i.setItem(2, new ItemBuilder(Material.WOOL).data((short)3).displayName("§bAqua").build());
-		i.setItem(3, new ItemBuilder(Material.WOOL).data((short)9).displayName("§3Cyan").build());
-		i.setItem(4, new ItemBuilder(Material.WOOL).data((short)13).displayName("§2Vert").build());
-		i.setItem(5, new ItemBuilder(Material.WOOL).data((short)5).displayName("§aVert Clair").build());
-		i.setItem(6, new ItemBuilder(Material.WOOL).data((short)6).displayName("§dRose").build());
-		i.setItem(7, new ItemBuilder(Material.WOOL).data((short)4).displayName("§eJaune").build());
-		i.setItem(8, new ItemBuilder(Material.WOOL).data((short)14).displayName("§cRouge").build());
-		i.setItem(9, new ItemBuilder(Material.WOOL).data((short)8).displayName("§7Gris").build());
+		i = Bukkit.createInventory(null, 18, "Â§aChoisis une couleur");
+		i.setItem(0, new ItemBuilder(Material.WOOL).data((short)1).displayName("Â§6Orange").build());
+		i.setItem(1, new ItemBuilder(Material.WOOL).data((short)11).displayName("Â§1Bleu").build());
+		i.setItem(2, new ItemBuilder(Material.WOOL).data((short)3).displayName("Â§bAqua").build());
+		i.setItem(3, new ItemBuilder(Material.WOOL).data((short)9).displayName("Â§3Cyan").build());
+		i.setItem(4, new ItemBuilder(Material.WOOL).data((short)13).displayName("Â§2Vert").build());
+		i.setItem(5, new ItemBuilder(Material.WOOL).data((short)5).displayName("Â§aVert Clair").build());
+		i.setItem(6, new ItemBuilder(Material.WOOL).data((short)6).displayName("Â§dRose").build());
+		i.setItem(7, new ItemBuilder(Material.WOOL).data((short)4).displayName("Â§eJaune").build());
+		i.setItem(8, new ItemBuilder(Material.WOOL).data((short)14).displayName("Â§cRouge").build());
+		i.setItem(9, new ItemBuilder(Material.WOOL).data((short)8).displayName("Â§7Gris").build());
 	}
 	
 	public static void open(Player p){
